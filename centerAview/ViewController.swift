e476fbf009596af75fae23fb354cddf8e5f7251d
westsider <whansen1@mac.com>
2021-07-12 17:29:50 -0500

    Initial Commit

---
+++ centerAview/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  centerAview
+//
+//  Created by Warren Hansen on 7/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
